39f1e3ccfc43a239b3564d188e42bb9dfbd6336a
Dan4ef <bdanilaf3@gmail.com>
2021-12-20 11:44:23 +0200

    1

--- main.java
+++
@@ -1,9 +0,0 @@
-class main {
-    public static void main(String[] args) {
-        int[] a = new int[100];
-
-        for (int i = 2; i < 100; i++)
-            a[i] = i + a[i - 1];
-        System.out.print(a);
-    }
-}
